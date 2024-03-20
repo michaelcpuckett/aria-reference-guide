fed6c9d7aeed65b865439482610193219bf04d71
Michael Puckett <michaelcpuckett@gmail.com>
2024-03-20 17:09:38 -0400

    data content tweaks

--- data/mappedAbstractAriaRolesToDescriptions.ts
+++ data/mappedAbstractAriaRolesToDescriptions.ts
@@ -1,11 +1,10 @@
 export const mappedAbstractAriaRolesToDescriptions = {
-  composite: "Represent containers for multiple interactive widgets.",
+  composite: "Represent containers for navigating interactive descendants.",
   landmark:
     "Represent top-level sections of a page that should appear in a site map or table of contents.",
-  structure:
-    "Represent non-interactive parts of a page with document semantics.",
+  structure: "Represent non-interactive parts of a page that organize content.",
   widget:
-    "Represent focusable, interactive elements. For user agents with different modes, this role switches the mode to keyboard passthrough.",
+    "Represent focusable, interactive elements. For user agents with different modes, these roles switches the mode to keyboard passthrough.",
   window: "Represent dialog windows that are children of the main window.",
   live: "Represent dynamic content or announcements that may be updated without user interaction.",
 };
